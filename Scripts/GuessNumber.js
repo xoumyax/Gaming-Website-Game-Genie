bc0e4e6d3b50bbc0f26b63b42a0477c01c1151d0
Soumyajyoti Dutta <47978486+Soumyajyotidutta@users.noreply.github.com>
2021-07-22 20:04:23 +0530

    Add files via upload

---
+++ Scripts/GuessNumber.js
@@ -0,0 +1,96 @@
+﻿'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+};
+
+// Again Event Listener
+document.querySelector('.again').addEventListener('click', async function () {
+    score = 20;
+
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = `Start guessing..`;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = null;
+
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('body').style.backgroundColor = '#222';
+
+    await fetch('http://localhost:2020/data', {
+        method: 'DELETE'
+    })
+
+});
+
+// Check Event Listener
+document.querySelector('.check').addEventListener('click', async function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        displayMessage(`🤦‍♂ No Number !`);
+
+        //When player wins
+    } else if (guess === secretNumber) {
+        displayMessage(`🎉 Correct Answer`);
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+
+        //HighScore
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+
+        // guess is not equal to secretNumber (Refracting the code)
+    } else if (guess !== secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent =
+                guess > secretNumber
+                    ? `☝ Guess is too high`
+                    : (document.querySelector(
+                        '.message'
+                    ).textContent = `👇 Guess is too low`);
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = `🐱‍👤Game Over`;
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+    console.log(score)
+    await fetch('http://localhost:2020/data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ score }),
+    });
+
+    const res = await fetch('http://localhost:2020/data');
+    const data = await res.json();
+    console.log(data.highScore);
+});
+function submit() {
+    var model = new Object();
+    model.UserId = '@us.UserId';
+    model.Score = score;
+    jQuery.ajax({
+        method: "POST",
+        url: '@Url.Action("Update")',
+        dataType: "json",
+        contentType: "application/json;charset=utf-8",
+        data: JSON.stringify({ std: model }),
+        success: function (data) {
+            alert('saved', 500);
+},
+failure: function () {
+    alert('error');
+}
+    })
+}
---
+++ Scripts/RPS.js
@@ -0,0 +1,93 @@
+﻿let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user_score");
+const computerScore_span = document.getElementById("computer_score");
+const scoreBoard_div = document.querySelector(".score_board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = (Math.floor(Math.random() * 3));
+    return choices[randomNumber];
+}
+
+function convertToWord(letter) {
+    if (letter === "r") return "Rock ";
+    if (letter === "p") return "Paper ";
+    return "Scissors ";
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(userChoice) + " " + " beats " + convertToWord(computerChoice) + "You Win";
+}
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(userChoice) + " loses " + convertToWord(computerChoice) + "You Lose...";
+
+}
+
+function draw(userChoice, computerChoice) {
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(userChoice) + " equals " + convertToWord(computerChoice) + "It's a Tie :-)";
+}
+
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
+
+function main() {
+
+    rock_div.addEventListener('click', function () {
+        game("r");
+    })
+
+    paper_div.addEventListener('click', function () {
+        game("p");
+    })
+
+    scissor_div.addEventListener('click', function () {
+        game("s");
+    })
+
+}
+function update() {
+    if (userScore == computerScore)
+        alert('Match Draw');
+    else if (userScore < computerScore)
+        alert('You Lost....');
+    else
+        alert('You Win....');
+}
+
+main();
+
